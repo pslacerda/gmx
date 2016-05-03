e38d013ebdb02aedf25678c88786f131860172f7
Pedro Lacerda <pslacerda@gmail.com>
2016-05-03 00:02:46 -0300

    rumo pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 from distutils.core import setup
 
+VERSION = '1'
 setup(
-    name = 'gmx-script',
-    version = '1',
+    name = 'gmx',
+    version = VERSION,
     description = 'Gromacs scripting framework',
     author = 'Pedro Sousa Lacerda',
     author_email = 'pslacerda+gmx@gmail.com',
     url = 'https://github.com/pslacerda/gmx',
+    download_url = 'https://github.com/pslacerda/gmx/tarball/%s' % VERSION,
     py_modules = ['gmx'],
  )

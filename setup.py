5b9117ca8ff0ace39e93ee02ea94cfd0807cc20c
Pedro Lacerda <pslacerda@gmail.com>
2016-05-02 23:51:41 -0300

    primeira versão

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(
+    name = 'gmx-script',
+    version = '1',
+    description = 'Gromacs scripting framework',
+    author = 'Pedro Sousa Lacerda',
+    author_email = 'pslacerda+gmx@gmail.com',
+    url = 'https://github.com/pslacerda/gmx',
+    py_modules = ['gmx'],
+ )
---
+++ test/sample.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+#
+# Sample molecular dynamics protocol
+#
+
+from gmx import *
+
+with system('1AKI'):
+
+    # System preparation
+    pdb2gmx(
+        ff    = 'gromos54a7',
+        water = 'spce',
+        f     = PDB['1AKI'],
+        o     = 'prot.gro',
+        p     = 'topol.top'
+    )
+
+    editconf(
+        f  = 'prot.gro',
+        o  = 'pbc.gro',
+        bt = 'dodecahedron',
+        d  = 1.4
+    )
+
+    solvate(
+        cp = 'pbc.gro',
+        cs = 'spc216.gro',
+        o  = 'sol.gro',
+        p  = 'topol.top'
+    )
+
+    grompp(
+        f = MDP['ions.mdp'],
+        c = 'sol.gro',
+        o = 'ions.tpr',
+        p = 'topol.top'
+    )
+
+    genion(
+        s       = 'ions.tpr',
+        o       = 'ions.gro',
+        neutral = 1,
+        p       = "topol.top",
+        stdin   = """
+            SOL
+        """
+    )
+
+    # Steep descent energy minimization
+    grompp(
+        f = MDP['em.mdp', {
+            'integrator': 'steep',
+            'emtol'     : 10.0,
+            'nsteps'    : 500,
+        }],
+        c = 'ions.gro',
+        o = 'sd.tpr',
+        p = 'topol.top'
+    )
+    mdrun(deffnm = 'sd')
+
+    # Conjugate gradient energy minimization
+    grompp(
+        f = MDP['em.mdp', {
+            'integrator': 'cg',
+            'emtol'     : 1.0,
+            'nsteps'    : 500,
+        }],
+        c = 'sd.gro',
+        o = 'cg.tpr',
+        p = 'topol.top'
+    )
+    mdrun(deffnm = 'cg')
+
+"""
+    # Position restrained molecular dynamics
+    grompp(
+        f = MDP['md.mdp', {
+            'steps'       : 250000,
+            'define'      : '-DPOSRES',
+            'continuation': False,
+            'gen_vel'     : True
+        }],
+        c = 'cg.gro',
+        o = 'mdposres.tpr',
+        p = 'topol.top'
+    )
+    mdrun(deffnm = 'mdposres')
+
+    # Molecular dynamics
+    grompp(
+        f = MDP['md.mdp', {
+            'steps'       : 25000,
+            'continuation': True,
+            'gen_vel'     : False
+        }],
+        c = 'mdposres.gro',
+        o = 'md.tpr',
+        p = 'topol.top'
+    )
+    mdrun(deffnm = 'md')
+"""
